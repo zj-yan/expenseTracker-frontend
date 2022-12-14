dbc77adbf36900cd9ecdfde9074a4b889bc39886
zhijie <yang.zhij@northeastern.edu>
2022-12-14 14:26:19 -0800

    initialize

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+reportWebVitals();
